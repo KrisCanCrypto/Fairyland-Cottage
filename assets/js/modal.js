0b261dccd60199263ab1ef79e547e6b3e731850a
Kris Can <69359836+KrisCanCrypto@users.noreply.github.com>
2025-01-11 12:09:24 +0000

    Feat: add model to contact page

---
+++ assets/js/modal.js
@@ -0,0 +1,22 @@
+document
+  .getElementById('contact-form')
+  .addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Show the modal
+    var modal = document.getElementById('emailSentModal');
+    modal.style.display = 'block';
+
+    // Close the modal when the user clicks on the close button
+    var closeButton = document.getElementsByClassName('close-button')[0];
+    closeButton.onclick = function () {
+      modal.style.display = 'none';
+    };
+
+    // Close the modal when the user clicks anywhere outside of the modal
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = 'none';
+      }
+    };
+  });
